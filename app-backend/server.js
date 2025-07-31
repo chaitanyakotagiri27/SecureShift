5739f43a507383a7038b56c181d02e54211e118a
Muhammad Musa <44801098+musahex@users.noreply.github.com>
2025-07-31 23:06:18 +1000

    Update server.js

--- app-backend/server.js
+++ app-backend/server.js
@@ -1,14 +1,30 @@
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/config/connectDB.js";
+import setupSwagger from "./src/config/swagger.js";
+
+// Load env variables
 dotenv.config();
 
-import app from './src/app.js';
-import connectDB from './src/config/db.js';
+const app = express();
 
-const PORT = process.env.PORT;
+// Connect to MongoDB
+connectDB();
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-    console.log(`SecureShift API running on port ${PORT}`);
-    console.log(`📘 Swagger UI available at: http://localhost:${PORT}/api-docs`);
-    });
+// Middleware
+app.use(express.json());
+
+// Swagger setup
+setupSwagger(app);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("SecureShift API is running.");
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`📘 Swagger UI available at: http://localhost:${PORT}/api-docs`);
 });
