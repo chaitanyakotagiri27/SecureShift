e21c64fb13487e7b1719a60f22e7f4771866b549
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2025-08-02 11:53:37 +1000

    Add EmployerDashboard and update App.js routing

--- app-frontend/employer-panel/src/App.js
+++ app-frontend/employer-panel/src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
-import CreateShift from './pages/createShift';// Import the CreateShift component
+import EmployerDashboard from './pages/EmployerDashboard'; 
+import CreateShift from './pages/createShift';
 
 function App() {
   return (
     <Router>
       <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
         <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
-        <Link to="/create-shift">Create Shift</Link> 
+        <Link to="/employer-dashboard">Dashboard</Link>
+        <Link to="/create-shift"style={{ marginLeft: '1rem' }}>Create Shift</Link> 
       </nav>
 
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/create-shift" element={<CreateShift />} />
+        <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+         <Route path="/create-shift" element={<CreateShift />} />
       </Routes>
     </Router>
   );
